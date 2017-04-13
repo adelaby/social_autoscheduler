3b18b3f4ed66f8923dfb89959c84d03c9b0b57ab
Anthony Delaby <pro@anthonydelaby.me>
2017-04-13 20:31:18 +0200

    Add django-categories to THIRD_PARTY_APPS

--- config/settings/base.py
+++ config/settings/base.py
@@ -52,6 +52,8 @@ THIRD_PARTY_APPS = [
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'categories',
+    'categories.editor',
 ]
 
 # Apps specific for this project go here.
