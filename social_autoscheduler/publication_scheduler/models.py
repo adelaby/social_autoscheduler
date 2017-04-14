c1f93de5d6e93bdd64f94da8bb1901bc0f4f47ba
Anthony Delaby <pro@anthonydelaby.me>
2017-04-14 12:12:54 +0200

    Add basic Publication model

--- social_autoscheduler/publication_scheduler/models.py
+++ social_autoscheduler/publication_scheduler/models.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.db import models
+from django.utils.text import Truncator
 
 
 class SocialNetwork(models.Model):
@@ -11,3 +13,21 @@ class SocialNetwork(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Publication(models.Model):
+    """Model representing a publication posted on a social network.
+
+    Attributes:
+        author (:obj:models.ForeignKey): user who posted the publication
+            (foreign key towards custom User model).
+        social_network (:obj:models.ForeignKey): social network the publication
+            belongs to (foreign key to `SocialNetwork` model).
+        content (:obj:models.TextField): text content of the publication.
+    """
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    social_network = models.ForeignKey(SocialNetwork)
+    content = models.TextField()
+
+    def __str__(self):
+        return Truncator(self.content).chars(89)
