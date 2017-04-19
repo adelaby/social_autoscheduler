bfb870bb609eb6f92b43d9835bbf68f70c9195e8
Anthony Delaby <pro@anthonydelaby.me>
2017-04-19 19:11:54 +0200

    Import publication url patterns

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
 
     # Your stuff: custom urls includes go here
+    url(r'publication/', include('social_autoscheduler.publication_scheduler.urls', namespace='publication')),
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
