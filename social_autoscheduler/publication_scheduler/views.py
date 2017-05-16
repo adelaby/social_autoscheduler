7e522fb488a29a4929071e07b757cba0f0365664
Anthony Delaby <pro@anthonydelaby.me>
2017-05-16 14:11:15 +0200

    Add `PublishEventCreate` view

--- social_autoscheduler/publication_scheduler/views.py
+++ social_autoscheduler/publication_scheduler/views.py
@@ -1,15 +1,23 @@
+import datetime
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.messages.views import SuccessMessageMixin
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, FormView
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from django_tables2 import SingleTableView
 
+from social_autoscheduler.publication_scheduler.forms import EventForm
 from social_autoscheduler.publication_scheduler.models import (Publication,
                                                                SocialNetwork,
                                                                Category)
 from social_autoscheduler.publication_scheduler.tables import PublicationTable
+
+
+def set_initial_social_network(initial_data):
+    if SocialNetwork.objects.count() == 1:
+        initial_data.update({'social_network': SocialNetwork.objects.get()})
 
 
 class CrispySubmitMixin(object):
@@ -52,8 +60,7 @@ class PublicationCreate(LoginRequiredMixin, SuccessMessageMixin,
             initial_data (dict): a dict in the form `{'field_name': field_value}`
         """
         initial_data = super().get_initial()
-        if SocialNetwork.objects.count() == 1:
-            initial_data.update({'social_network': SocialNetwork.objects.get()})
+        set_initial_social_network(initial_data)
         return initial_data
 
     def form_valid(self, form):
@@ -78,7 +85,7 @@ class PublicationList(LoginRequiredMixin, SingleTableView):
         """Filters and returns all publications created by current user.
 
         Returns:
-            _ (:obj:QuerySet): a Django `QuerySet` instance.
+            _ (:obj:`QuerySet`): a Django `QuerySet` instance.
         """
         return Publication.objects.filter(author=self.request.user)
 
@@ -98,4 +105,37 @@ class CategoryCreate(LoginRequiredMixin, SuccessMessageMixin,
         success url.
         """
         form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+
+class PublishEventCreate(LoginRequiredMixin, SuccessMessageMixin,
+                         CrispySubmitMixin, FormView):
+    """View managing the creation of a `PublishEvent` instance.
+    """
+
+    template_name = 'publication_scheduler/rule_form.html'
+    form_class = EventForm
+    success_url = '/'
+    success_message = 'Rule created successfully'
+
+    def get_initial(self):
+        """Generates initial data and adds current weekday, time and social
+        network to it.
+        """
+        now = datetime.datetime.now()
+        initial_data = {'weekday': now.weekday(), 'time': now.time()}
+        set_initial_social_network(initial_data)
+        return initial_data
+
+    def get_form_kwargs(self):
+        """Generates form kwargs and adds current user to it.
+        """
+        form_kwargs = super().get_form_kwargs()
+        form_kwargs['user'] = self.request.user
+        return form_kwargs
+
+    def form_valid(self, form):
+        """Ensures event creation before rendering response.
+        """
+        form.get_or_create_event()
         return super().form_valid(form)
